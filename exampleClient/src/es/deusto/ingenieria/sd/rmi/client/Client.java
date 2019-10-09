41194073c56e55ae36e46d36ffdb56f37ece2d61
Mikel Moreno Moreno <43069596+miikel23@users.noreply.github.com>
2019-10-09 19:53:46 +0200

    version 1.2 (includes scanner for interaction)

--- exampleClient/src/es/deusto/ingenieria/sd/rmi/client/Client.java
+++ exampleClient/src/es/deusto/ingenieria/sd/rmi/client/Client.java
@@ -1,5 +1,7 @@
 package es.deusto.ingenieria.sd.rmi.client;
 
+import java.util.Scanner;
+
 import es.deusto.ingenieria.sd.rmi.server.IServer;
 
 public class Client {
@@ -13,14 +15,24 @@ public class Client {
 		if (System.getSecurityManager() == null) {
 			System.setSecurityManager(new SecurityManager());
 		}
-
+		
 		try {
 			String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
 			IServer stubServer = (IServer) java.rmi.Naming.lookup(name);
+			//here i start editing
+			String nombre, pass, message;
+			Scanner scan = new Scanner(System.in);
 			System.out.println("* Message coming from the server: '" + stubServer.sayHello() + "'");
+			System.out.println("Please enter your username");
+			nombre = scan.next();
+			System.out.println("Please enter your password");
+			pass= scan.next();
 			System.out.println("registering user");
-			stubServer.registerUser( "mikel",  "moreno" );
-			System.out.println("* Message coming from the server: '" + stubServer.sayMessage( "mikel", "moreno", "buenos dias mundo") + "'");
+			stubServer.registerUser( nombre,  pass );
+			System.out.println("write a message");
+			message = scan.next();
+			System.out.println("* Message coming from the server: '" + stubServer.sayMessage( nombre, pass, message) + "'");
+			//stop editing
 			
 		} catch (Exception e) {
 			System.err.println("- Exception running the client: " + e.getMessage());
